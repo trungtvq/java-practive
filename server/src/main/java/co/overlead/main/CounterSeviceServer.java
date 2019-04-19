0e5a8754b31b976cc0f575666bf2b3b677ccad2a
trungtvq <trungtvq.share@gmail.com>
2019-04-19 13:06:44 +0700

    update conter

--- server/src/main/java/co/overlead/main/CounterSeviceServer.java
+++ server/src/main/java/co/overlead/main/CounterSeviceServer.java
@@ -10,30 +10,37 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CounterSeviceServer {
-    private static final Logger logger = LogManager.getLogger(CounterSeviceServer.class.getName());
-
+    //private static final Logger logger = LogManager.getLogger(CounterSeviceServer.class.getName());
+    private static HashMap<String,Long> cacheBalance=new HashMap<>();
+    private static QueueReq queueReq=new QueueReq();
+    public static HashMap getCacheBalance(){
+        return cacheBalance;
+    }
     private Server server;
-
     private void start() throws IOException {
         /* The port on which the server should run */
 
-        logger.trace("tracing... ...");
-        logger.debug("debuging ... ...");
-
-        logger.info("info ... ... ");
-        logger.warn("warning .... ... ");
-        logger.error("error ... ... ");
-        logger.fatal("fatal ... ...");
+//        logger.trace("tracing... ...");
+//        logger.debug("debuging ... ...");
+//
+//        logger.info("info ... ... ");
+//        logger.warn("warning .... ... ");
+//        logger.error("error ... ... ");
+//        logger.fatal("fatal ... ...");
 
         int port = 9090;
         server = ServerBuilder.forPort(port)
                 .addService(new CounterServiceImpl())
                 .build()
                 .start();
-        logger.info("Server started, listening on " + port);
+        // logger.info("Server started, listening on " + port);
 
+        /* Add hook when stop application*/
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
             public void run() {
@@ -62,50 +69,32 @@ public class CounterSeviceServer {
         }
     }
 
-    //service CounterService{
-    //    rpc SetBalance(UserReq) returns (BalanceRes);
-    //    rpc GetBalance(UserReq) returns (BalanceRes);
-    //    rpc IncreaseBalance(UserReq) returns (BalanceRes);
-    //    rpc DecreaseBalance(UserReq) returns (BalanceRes);
-    //}
-    //
-    //message UserReq{
-    //    string userId = 1;
-    //    int64 balance = 2;
-    //}
-    //
-    //message BalanceRes{
-    //    int64 balance = 1;
-    //}
-    /** Say hello to server. */
-    /**
-     * Main launches the server from the command line.
-     */
+
     public static void main(String[] args) throws IOException, InterruptedException {
         final CounterSeviceServer server = new CounterSeviceServer();
         server.start();
-        server.blockUntilShutdown();
+        server.blockUntilShutdown(); //prevent application shutdown
+
+//        for (int i = 0; i < 10; i++) {
+//            Runnable worker = new SendThread("" + i);
+//            executor.execute(worker);//calling execute method of ExecutorService
+//        }
+
+        //executor.shutdown();
+        //while (!executor.isTerminated()) {   }
+
+        System.out.println("Finished all threads");
     }
 
     static class CounterServiceImpl extends CounterServiceGrpc.CounterServiceImplBase {
 
 
+
         @Override
         public void getBalance(Counterservice.UserReq req, StreamObserver<Counterservice.BalanceRes> responseObserver){
 
-            Long value=0L;
-            if (IRedis.USER_SYNC_COMMAND.get(req.getUserId())==null){
-                Counterservice.BalanceRes reply= Counterservice.BalanceRes.newBuilder().setBalance(0).build();
 
-                responseObserver.onNext(reply);
-                responseObserver.onCompleted();
-            } else{
-                value=Long.parseLong(IRedis.USER_SYNC_COMMAND.get(req.getUserId()).toString());
-                Counterservice.BalanceRes reply= Counterservice.BalanceRes.newBuilder().setBalance(value).build();
-
-                responseObserver.onNext(reply);
-                responseObserver.onCompleted();
-            }
+            queueReq.addKey(req.getUserId(),0L,"GET",responseObserver);
 
         }
 
@@ -113,66 +102,19 @@ public class CounterSeviceServer {
         @Override
         public void increaseBalance(Counterservice.UserReq req, StreamObserver<Counterservice.BalanceRes> responseObserver){
 
-            if (IRedis.USER_SYNC_COMMAND.get(req.getUserId())==null){
-                Long value=req.getBalance();
-                IRedis.USER_SYNC_COMMAND.set(req.getUserId(),value.toString());
-                Counterservice.BalanceRes reply= Counterservice.BalanceRes.newBuilder().setBalance(req.getBalance()).build();
-                responseObserver.onNext(reply);
-                responseObserver.onCompleted();
-            } else{
+            queueReq.addKey(req.getUserId(),req.getBalance(),"INCR",responseObserver);
 
-                Long value= (Long) req.getBalance();
-
-                // IRedis.USER_SYNC_COMMAND.set(req.getUserId(),newVal.toString());
-                IRedis.USER_SYNC_COMMAND.incrby(req.getUserId(),value);
-                Counterservice.BalanceRes reply= Counterservice.BalanceRes.newBuilder().setBalance(value).build();
-                responseObserver.onNext(reply);
-                responseObserver.onCompleted();
-            }
         }
 
         @Override
         public void decreaseBalance(Counterservice.UserReq req, StreamObserver<Counterservice.BalanceRes> responseObserver){
-
-            if (IRedis.USER_SYNC_COMMAND.get(req.getUserId())==null){
-                Long value=-req.getBalance();
-                IRedis.USER_SYNC_COMMAND.set(req.getUserId(),value.toString());
-
-                Counterservice.BalanceRes reply= Counterservice.BalanceRes.newBuilder().setBalance(req.getBalance()).build();
-                responseObserver.onNext(reply);
-                responseObserver.onCompleted();
-            } else{
-
-
-                Long value=req.getBalance();
-
-                Counterservice.BalanceRes reply= Counterservice.BalanceRes.newBuilder().setBalance(value).build();
-                IRedis.USER_SYNC_COMMAND.decrby(req.getUserId(),value);
-                responseObserver.onNext(reply);
-                responseObserver.onCompleted();
-            }
+            queueReq.addKey(req.getUserId(),req.getBalance(),"DECR",responseObserver);
         }
 
         @Override
         public void setBalance(Counterservice.UserReq req, StreamObserver<Counterservice.BalanceRes> responseObserver){
-            Long value=0L;
-            if (IRedis.USER_SYNC_COMMAND.get(req.getUserId())==null){
+            queueReq.addKey(req.getUserId(),req.getBalance(),"SET",responseObserver);
 
-                Long val=req.getBalance();
-                IRedis.USER_SYNC_COMMAND.set(req.getUserId(),val.toString());
-
-                Counterservice.BalanceRes reply= Counterservice.BalanceRes.newBuilder().setBalance(req.getBalance()).build();
-
-                responseObserver.onNext(reply);
-                responseObserver.onCompleted();
-
-            } else{
-                Long newVal=req.getBalance();
-                Counterservice.BalanceRes reply= Counterservice.BalanceRes.newBuilder().setBalance(newVal).build();
-                IRedis.USER_SYNC_COMMAND.set(req.getUserId(),newVal.toString());
-                responseObserver.onNext(reply);
-                responseObserver.onCompleted();
-            }
         }
     }
 }
